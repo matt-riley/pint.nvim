a71f9ce48b1e311b71044a8575a5e36b54d7e6a2
Matt Riley <matt-riley@users.noreply.github.com>
2026-07-11 19:06:57 +0100

    feat: add Pint lifecycle commands

--- plugin/pint.lua
+++ plugin/pint.lua
@@ -1,12 +1,29 @@
 -- plugin/pint.lua
 -- Auto-loaded by Neovim. Registers the :Pint user command.
 
+local subcommands = {
+  "dashboard",
+  "history",
+  "dismiss",
+  "dismiss-all",
+  "restore",
+  "words-enable",
+  "words-disable",
+}
+
 vim.api.nvim_create_user_command("Pint", function(cmd)
   local sub = cmd.fargs[1] or "dashboard"
+
   if sub == "dashboard" then
     require("pint.dashboard").open()
   elseif sub == "history" then
     require("pint.notifier").show_history()
+  elseif sub == "dismiss" then
+    require("pint.notifier").dismiss()
+  elseif sub == "dismiss-all" then
+    require("pint.notifier").dismiss_all()
+  elseif sub == "restore" then
+    require("pint").restore()
   elseif sub == "words-enable" then
     require("pint.words").enable()
   elseif sub == "words-disable" then
@@ -18,6 +35,6 @@ end, {
   desc = "pint.nvim",
   nargs = "?",
   complete = function()
-    return { "dashboard", "history", "words-enable", "words-disable" }
+    return subcommands
   end,
 })
